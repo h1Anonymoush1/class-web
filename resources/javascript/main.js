ce58c7f9a25b08b8c527429b1b78b957c212e0f6
Shimmy kohn <116236681+h1anonymoush1@users.noreply.github.com>
2022-12-14 22:26:48 -0500

    added password

---
+++ resources/javascript/main.js
@@ -0,0 +1,31 @@
+/*function hideChat() {
+    document.getElementById('chat').hidden = true;
+}
+
+function showChat() {
+    document.getElementById('chat').hidden = false;
+}
+
+const password = 2007;
+
+let userInput = prompt('Enter in the password ',' ');
+
+if (userInput == password) {
+    return showChat()
+} else {
+    return hideChat()
+}
+
+
+document.addEventListener('click', hideChat);*/
+
+
+var password = 2007;
+var x = prompt("Enter in the password "," ");
+if (x.toLowerCase() == password) {
+ alert("Come right in \n \n You've entered in the right password");
+ window.location = "index1.html";
+}
+else {
+ window.location = "bad.htm";
+}
