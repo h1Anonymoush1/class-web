883ea91d822b73b6c6cb50da339d3f549bc0db52
Shimmy kohn <116236681+h1anonymoush1@users.noreply.github.com>
2022-12-15 21:15:24 -0500

    commented alerts for login

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -23,9 +23,10 @@ document.addEventListener('click', hideChat);*/
 var password = 2007;
 var x = prompt("Enter in the password "," ");
 if (x.toLowerCase() == password) {
- alert("Come right in \n \n You've entered in the right password");
+ //alert("Come right in \n \n You've entered in the right password");
  window.location = "index1.html";
 }
 else {
- window.location = "bad.htm";
+//alert("please try again")
+ window.location = "";
 }
