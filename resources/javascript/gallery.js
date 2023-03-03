96851fb57233021c1591b8523397a07970f6ff0d
Shimmy kohn <116236681+h1anonymoush1@users.noreply.github.com>
2023-03-03 16:40:08 -0500

    added media quary in gallery and songs

--- resources/javascript/gallery.js
+++ resources/javascript/gallery.js
@@ -4,6 +4,12 @@ const arrow1 = document.getElementById('arrow1');
 
 const nlinks = document.getElementById('nlinks');
 
+const headerH1 = document.getElementById('headerh1');
+
+var x = window.matchMedia("(max-width: 786px)")
+ipad(x) 
+x.addListener(ipad)
+
 function hideLinks(){
     nlinks.style.display='none';
     arrow.style.display='none';
@@ -18,3 +24,13 @@ function showLinks(){
 
 arrow.addEventListener('click', hideLinks);
 arrow1.addEventListener('click', showLinks);
+
+//media quary ipad
+function ipad(x) {
+    if (x.matches) { 
+      nlinks.style.display='none';
+      arrow.style.display='none';
+      arrow1.style.display='inline';
+      arrow.style.right='16rem';
+    }
+  }
